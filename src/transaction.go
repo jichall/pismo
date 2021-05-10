f5fbbfea5fafed01f4dd948d07a5037c8366ab62
Rafael Campos Nunes <rafaelnunes@engineer.com>
2021-05-10 19:38:07 -0300

    Add conversors fro amount and time fields
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/transaction.go
+++ src/transaction.go
@@ -3,20 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"time"
 )
 
 type Transaction struct {
-	ID      int
-	Amount  string `json:"amount"`
-	Date    time.Time
-	Account int `json:"account_id"`
-	Type    int `json:"operation_type_id"`
+	ID      int       `json:"id"`
+	Amount  float64   `json:"amount"`
+	Date    time.Time `json:"timestamp"`
+	Account int       `json:"account_id"`
+	Type    int       `json:"operation_type_id"`
 }
 
 func (t *Transaction) String() string {
 
-	format := "Transaction<id: %d amount: %s account_id: %d type: %d time: %v>"
+	format := "Transaction<id: %d amount: %f account_id: %d type: %d time: %v>"
 
 	return fmt.Sprintf(format, t.ID, t.Amount, t.Account, t.Type, t.Date)
 }
@@ -29,8 +30,11 @@ func (t *Transaction) Insert() error {
 	}
 	defer conn.Release()
 
-	_, err = conn.Query(context.Background(), insertTransaction,
-		[]interface{}{t.Amount, time.Now(), t.Account, t.Type})
+	amount := strconv.FormatFloat(t.Amount, 'f', 5, 64)
+	timestamp := time.Now().Format("2006-01-02T15:04:05-0700")
+
+	_, err = conn.Exec(context.Background(), insertTransaction,
+		amount, timestamp, t.Account, t.Type)
 
 	return err
 }
